13003fd77396315434ac2907bc28650c03c69b1b
Paweł Martosz <pawel.m476@gmail.com>
2022-05-24 18:57:17 +0200

    ok

---
+++ MongoDBMVC/Data/Model/Computers.cs
@@ -0,0 +1,36 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Model
+{
+    public class Computers
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        private string FinallyId;
+
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id
+        {
+            get { return FinallyId; }
+            set { if (value == null) { FinallyId = ObjectId.GenerateNewId().ToString(); } else { FinallyId = value; } }
+        }
+
+        [Display(Name="Computer name")]
+        public string Name { get; set; }
+
+        [Display(Name ="Creation year")]
+        public int Year { get; set; }
+        
+        public string ImageId { get; set; }
+
+        public bool HasImage()=> !string.IsNullOrEmpty(ImageId);    
+
+        
+    }
+}
